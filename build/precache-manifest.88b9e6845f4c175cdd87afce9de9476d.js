9a857593f45446d47e4a6d6e5d008889f2b11bec
JRathbone <justinrathbone@gmail.com>
2019-09-29 15:22:59 -0500

    FirstReactApp

---
+++ build/precache-manifest.88b9e6845f4c175cdd87afce9de9476d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21691f9b8f09b19861c53c2305cb4d35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dd4269bb9399ce919aa",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5e8f5fc55b1d46768881",
+    "url": "/static/css/main.4e5af744.chunk.css"
+  },
+  {
+    "revision": "7dd4269bb9399ce919aa",
+    "url": "/static/js/2.0a6ad8b6.chunk.js"
+  },
+  {
+    "revision": "5e8f5fc55b1d46768881",
+    "url": "/static/js/main.970cb6b5.chunk.js"
+  },
+  {
+    "revision": "720e22d81d6d677b3a62",
+    "url": "/static/js/runtime-main.c26e8b41.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
